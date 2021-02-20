b09035b372fbd543baece4e9c4a28c839614ede7
cpustejovsky <charles.pustejovsky@gmail.com>
2021-02-20 18:58:37 -0500

    Simplify SortInts with `sort.IntSlice{}`

--- number.go
+++ number.go
@@ -2,20 +2,7 @@ package customsortgo
 
 import "sort"
 
-type SortIntList []int
-
-func (n SortIntList) Len() int {
-	return len(n)
-}
-func (n SortIntList) Swap(i, j int) {
-	n[i], n[j] = n[j], n[i]
-}
-func (n SortIntList) Less(i, j int) bool {
-	return n[i] < n[j]
-}
-
-
 func SortInts(nums []int) []int {
-	sort.Sort(SortIntList(nums))
+	sort.Sort(sort.IntSlice(nums))
 	return nums
 }
