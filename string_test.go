74de910bb541a2054b421e06c91590431f76b7b8
cpustejovsky <charles.pustejovsky@gmail.com>
2021-02-20 17:28:46 -0500

    Add test for ReverseSortWords

--- string_test.go
+++ string_test.go
@@ -2,14 +2,24 @@ package string_test
 
 import (
 	"testing"
-	th "github.com/cpustejovsky/customsortgo/testhelp"
+
 	csgo "github.com/cpustejovsky/customsortgo"
+	th "github.com/cpustejovsky/customsortgo/testhelp"
 )
 
-func TestSortWords(t *testing.T){
+func TestSortWords(t *testing.T) {
 	w := []string{"cat", "albatross", "dolphin", "bee", "zebra", "aardvark"}
 	want := []string{"aardvark", "albatross", "bee", "cat", "dolphin", "zebra"}
 	got := csgo.SortWords(w)
+
+	th.AssertEqual(t, got, want)
+
+}
+
+func TestReverseSortWords(t *testing.T) {
+	w := []string{"cat", "albatross", "dolphin", "bee", "zebra", "aardvark"}
+	want := []string{"zebra", "dolphin", "cat", "bee", "albatross", "aardvark"}
+	got := csgo.ReverseSortWords(w)
 
 	th.AssertEqual(t, got, want)
 
