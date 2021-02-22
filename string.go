91ee2c68ae92b7d84c2de9e76629df0754337df3
cpustejovsky <charles.pustejovsky@gmail.com>
2021-02-22 11:39:40 -0500

    Replace ranging to create rune slice with type conversion

--- string.go
+++ string.go
@@ -1,10 +1,7 @@
 package customsortgo
 
 func ReverseString(word string) string {
-	var r []rune
-	for _, v := range word {
-		r = append(r, v)
-	}
+	r := []rune(word)
 	var rw []rune
 	for i := len(r) - 1; i >= 0; i-- {
 		rw = append(rw, r[i])
