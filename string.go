dbc22680a57458adea633999d4dfb1e66c4e7f71
cpustejovsky <charles.pustejovsky@gmail.com>
2021-02-22 12:20:02 -0500

    Set up benchmark to determine modifying a single rune slice was faster

--- string.go
+++ string.go
@@ -2,9 +2,18 @@ package customsortgo
 
 func ReverseString(word string) string {
 	r := []rune(word)
+	for i := 0; i < len(r)/2; i++ {
+		r[i], r[len(r)-1-i] = r[len(r)-1-i], r[i]
+	}
+	return string(r)
+}
+
+func ReverseStringSlow(word string) string {
+	r := []rune(word)
 	var rw []rune
 	for i := len(r) - 1; i >= 0; i-- {
 		rw = append(rw, r[i])
 	}
 	return string(rw)
 }
+
--- string_test.go
+++ string_test.go
@@ -13,3 +13,18 @@ func TestReverseString(t *testing.T) {
 	got := csgo.ReverseString(w)
 	th.AssertEqual(t, got, want)
 }
+
+var longStr = "A9Fn3WqFTqhyH8t&K$rPZ5NCsYCQgzPGT&7X%Acj#NB69YnyV!YLrNNCQq3s%fxs9okr5&XcT*GXMVEajc3RWo^XHx4Zx2A*5WCHjecbcLWCY2VAf6g6P7aeg^FZfBFq"
+
+func BenchmarkReverseString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		csgo.ReverseString(longStr)
+	}
+}
+func BenchmarkReverseStringSlow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		csgo.ReverseStringSlow(longStr)
+	}
+}
+
+
