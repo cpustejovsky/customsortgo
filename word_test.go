06807649dd6f81db70af685e189e21bf59fc2df2
cpustejovsky <charles.pustejovsky@gmail.com>
2021-02-22 18:56:28 -0500

    Add tests for Chars methods

--- word_test.go
+++ word_test.go
@@ -7,6 +7,26 @@ import (
 	th "github.com/cpustejovsky/customsortgo/testhelp"
 )
 
+var testChars csgo.Chars = []rune("hello, world!")
+
+func TestChars(t *testing.T) {
+	wantLen := 13
+	gotLen := testChars.Len()
+	th.AssertEqual(t, gotLen, wantLen)
+
+	wantLessFirst := false
+	gotLessFirst := testChars.Less(0, 1)
+	th.AssertEqual(t, gotLessFirst, wantLessFirst)
+
+	wantSwap := "ehllo, world!"
+	testChars.Swap(0, 1)
+	th.AssertEqual(t, string(testChars), wantSwap)
+
+	wantLessSecond := true
+	gotLessSecond := testChars.Less(0, 1)
+	th.AssertEqual(t, gotLessSecond, wantLessSecond)
+}
+
 func TestSortWord(t *testing.T) {
 	w := "cbadegf"
 	want := "abcdefg"
