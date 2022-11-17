d958dbe5995b848ac005d99525ccf52ee32bb0f9
cpustejovsky <charles.pustejovsky@gmail.com>
2022-11-17 18:56:02 -0500

    feat: split up Chars method tests

--- word/word.go
+++ word/word.go
@@ -1,10 +1,10 @@
-package customsortgo
+package word
 
 import (
 	"sort"
 )
 
-//Chars type is what a string is converted into to act as a sort Interface
+// Chars type is what a string is converted into to act as a sort Interface
 type Chars []rune
 
 func (ch Chars) Len() int {
@@ -17,14 +17,14 @@ func (ch Chars) Less(i, j int) bool {
 	return ch[i] < ch[j]
 }
 
-//SortWord takes a string and returns it with it's characters in a sorted order
+// SortWord takes a string and returns it with its characters in a sorted order
 func SortWord(word string) string {
 	ch := Chars(word)
 	sort.Sort(ch)
 	return string(ch)
 }
 
-//ReverseSortWord takes a string and returns it with it's characters in a reverse sorted order
+// ReverseSortWord takes a string and returns it with its characters in a reverse sorted order
 func ReverseSortWord(word string) string {
 	ch := Chars(word)
 	sort.Sort(sort.Reverse(ch))
--- word/word_test.go
+++ word/word_test.go
@@ -1,27 +1,31 @@
-package customsortgo_test
+package word_test
 
 import (
+	csgo "github.com/cpustejovsky/customsortgo/word"
 	"github.com/stretchr/testify/assert"
 	"testing"
-
-	csgo "github.com/cpustejovsky/customsortgo"
 )
 
-var testChars csgo.Chars = []rune("hello, world!")
-
-func TestChars(t *testing.T) {
+func TestChars_Len(t *testing.T) {
+	var testChars csgo.Chars = []rune("hello, world!")
 	wantLen := 13
 	gotLen := testChars.Len()
 	assert.Equal(t, wantLen, gotLen)
+}
 
-	gotLessFirst := testChars.Less(0, 1)
-	assert.Equal(t, false, gotLessFirst)
-
+func TestChars_Swap(t *testing.T) {
+	var testChars csgo.Chars = []rune("hello, world!")
 	wantSwap := "ehllo, world!"
 	testChars.Swap(0, 1)
 	gotSwap := string(testChars)
 	assert.Equal(t, wantSwap, gotSwap)
+}
 
+func TestChars_Less(t *testing.T) {
+	var testChars csgo.Chars = []rune("hello, world!")
+	gotLessFirst := testChars.Less(0, 1)
+	assert.Equal(t, false, gotLessFirst)
+	testChars.Swap(0, 1)
 	gotLessSecond := testChars.Less(0, 1)
 	assert.Equal(t, true, gotLessSecond)
 }
@@ -31,13 +35,11 @@ func TestSortWord(t *testing.T) {
 	want := "abcdefg"
 	got := csgo.SortWord(w)
 	assert.Equal(t, want, got)
-
 }
 
 func TestReverseSortWord(t *testing.T) {
 	w := "cbadegf"
 	want := "gfedcba"
 	got := csgo.ReverseSortWord(w)
-
 	assert.Equal(t, want, got)
 }
