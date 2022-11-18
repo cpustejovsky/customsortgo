732516dd9c0942a69aaae7b24932756fdd171dcd
cpustejovsky <charles.pustejovsky@gmail.com>
2022-11-17 19:02:02 -0500

    feat: Replace interface sorting functions with functions that construct new sorted or reverse sorted slices This prevents side effects

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,7 +1,7 @@
-package customsortgo_test
+package sort_test
 
 import (
-	csgo "github.com/cpustejovsky/customsortgo"
+	csgo "github.com/cpustejovsky/customsortgo/sort"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,59 +9,41 @@ import (
 func TestSortForStrings(t *testing.T) {
 	w := []string{"cat", "albatross", "dolphin", "bee", "zebra", "aardvark"}
 	want := []string{"aardvark", "albatross", "bee", "cat", "dolphin", "zebra"}
-	got, err := csgo.Sort(w)
-	assert.Nil(t, err)
+	got := csgo.NewSortedStrings(w)
 	assert.Equal(t, want, got)
 }
 
 func TestReverseSortForStrings(t *testing.T) {
 	w := []string{"cat", "albatross", "dolphin", "bee", "zebra", "aardvark"}
 	want := []string{"zebra", "dolphin", "cat", "bee", "albatross", "aardvark"}
-	got, err := csgo.ReverseSort(w)
-	assert.Nil(t, err)
+	got := csgo.NewReverseSortedStrings(w)
 	assert.Equal(t, want, got)
 }
 
 func TestSortInts(t *testing.T) {
 	n := []int{1, 3, 5, 2, 7, 6, 4}
 	want := []int{1, 2, 3, 4, 5, 6, 7}
-	got, err := csgo.Sort(n)
-	assert.Nil(t, err)
+	got := csgo.NewSortedIntegers(n)
 	assert.Equal(t, want, got)
 }
 
 func TestReverseSortInts(t *testing.T) {
 	n := []int{1, 3, 5, 2, 7, 6, 4}
 	want := []int{7, 6, 5, 4, 3, 2, 1}
-	got, err := csgo.ReverseSort(n)
-	assert.Nil(t, err)
+	got := csgo.NewReverseSortedIntegers(n)
 	assert.Equal(t, want, got)
 }
 
 func TestSortFloat64s(t *testing.T) {
 	n := []float64{1.1, 3.3, 5.5, 2.2, 7.7, 6.6, 4.4}
 	want := []float64{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7}
-	got, err := csgo.Sort(n)
-	assert.Nil(t, err)
+	got := csgo.NewSortedFloats(n)
 	assert.Equal(t, want, got)
 }
 
 func TestReverseSortFloat64s(t *testing.T) {
 	n := []float64{1.1, 3.3, 5.5, 2.2, 7.7, 6.6, 4.4}
 	want := []float64{7.7, 6.6, 5.5, 4.4, 3.3, 2.2, 1.1}
-	got, err := csgo.ReverseSort(n)
-	assert.Nil(t, err)
+	got := csgo.NewReverseSortedFloats(n)
 	assert.Equal(t, want, got)
-}
-
-type testStruct struct {
-	Foo string
-}
-
-func TestSortError(t *testing.T) {
-	ts := testStruct{Foo: "Bar"}
-	c := []testStruct{ts, ts, ts}
-	_, err := csgo.Sort(c)
-	check := &csgo.IncorrectTypeError{}
-	assert.ErrorAs(t, err, &check)
 }
