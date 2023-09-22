946572d4c13de4ef7c3e642065fa79145ed62ba3
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2023-09-22 14:18:36 -0400

    feat: remove testify from word-test.go and candlestick_test.go

--- word/word_test.go
+++ word/word_test.go
@@ -2,44 +2,55 @@ package word_test
 
 import (
 	csgo "github.com/cpustejovsky/customsortgo/word"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestChars_Len(t *testing.T) {
 	var testChars csgo.Chars = []rune("hello, world!")
-	wantLen := 13
-	gotLen := testChars.Len()
-	assert.Equal(t, wantLen, gotLen)
+	want := 13
+	got := testChars.Len()
+	if want != got {
+		t.Errorf("got:\n%v\nwanted:\n%v\n", got, want)
+	}
 }
 
 func TestChars_Swap(t *testing.T) {
 	var testChars csgo.Chars = []rune("hello, world!")
-	wantSwap := "ehllo, world!"
+	want := "ehllo, world!"
 	testChars.Swap(0, 1)
-	gotSwap := string(testChars)
-	assert.Equal(t, wantSwap, gotSwap)
+	got := string(testChars)
+	if want != got {
+		t.Errorf("got:\n%v\nwanted:\n%v\n", got, want)
+	}
 }
 
 func TestChars_Less(t *testing.T) {
 	var testChars csgo.Chars = []rune("hello, world!")
 	gotLessFirst := testChars.Less(0, 1)
-	assert.Equal(t, false, gotLessFirst)
+	if gotLessFirst {
+		t.Error("should return false")
+	}
 	testChars.Swap(0, 1)
 	gotLessSecond := testChars.Less(0, 1)
-	assert.Equal(t, true, gotLessSecond)
+	if !gotLessSecond {
+		t.Error("should return true")
+	}
 }
 
 func TestSortWord(t *testing.T) {
 	w := "cbadegf"
 	want := "abcdefg"
 	got := csgo.SortWord(w)
-	assert.Equal(t, want, got)
+	if want != got {
+		t.Errorf("got:\n%v\nwanted:\n%v\n", got, want)
+	}
 }
 
 func TestReverseSortWord(t *testing.T) {
 	w := "cbadegf"
 	want := "gfedcba"
 	got := csgo.ReverseSortWord(w)
-	assert.Equal(t, want, got)
+	if want != got {
+		t.Errorf("got:\n%v\nwanted:\n%v\n", got, want)
+	}
 }
