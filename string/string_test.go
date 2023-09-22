cd98acfd979b9e8a2a4b76cec98f40a39a8fc3b1
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2023-09-22 14:08:36 -0400

    feat: remove testify from string_test.go and add test for slow func

--- string/string_test.go
+++ string/string_test.go
@@ -2,7 +2,6 @@ package string_test
 
 import (
 	csgo "github.com/cpustejovsky/customsortgo/string"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -10,7 +9,18 @@ func TestReverseString(t *testing.T) {
 	w := "abdc"
 	want := "cdba"
 	got := csgo.ReverseString(w)
-	assert.Equal(t, want, got)
+	if want != got {
+		t.Errorf("got:\n%v\nwanted:\n%v\n", got, want)
+	}
+}
+
+func TestReverseStringSlow(t *testing.T) {
+	w := "abdc"
+	want := "cdba"
+	got := csgo.ReverseStringSlow(w)
+	if want != got {
+		t.Errorf("got:\n%v\nwanted:\n%v\n", got, want)
+	}
 }
 
 var longStr = "A9Fn3WqFTqhyH8t&K$rPZ5NCsYCQgzPGT&7X%Acj#NB69YnyV!YLrNNCQq3s%fxs9okr5&XcT*GXMVEajc3RWo^XHx4Zx2A*5WCHjecbcLWCY2VAf6g6P7aeg^FZfBFq"
